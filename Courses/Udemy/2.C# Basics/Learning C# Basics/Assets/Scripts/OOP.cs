b6e89ed9b6fe660d39d4fbffd659c68956b156e1
Mihail Stanescu <mstanescu@ausy-group.com>
2018-02-20 13:50:52 +0200

    C# Basics course done
    Signed-off-by: Mihail Stanescu

---
+++ Courses/Udemy/2.C# Basics/Learning C# Basics/Assets/Scripts/OOP.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript1 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        Human h1 = new Human(15, "Dorel", "student");
+        Human h2 = new Human(30, "Sissi", "architect");
+
+        Debug.Log(h1.name);
+
+        h1.Work();
+        h2.Work();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public class Human
+    {
+        int age;
+        public string name;
+        string job;
+
+        public Human(int myAge, string myName, string myJob)
+        {
+            age = myAge;
+            name = myName;
+            job = myJob;
+        }
+
+        public void Work()
+        {
+            Debug.Log("I'm " + name + " and my job is " + job);
+        }
+    }
+}
---
+++ Courses/Udemy/2.C# Basics/Learning C# Basics/Assets/Scripts/Scope.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Scope : MonoBehaviour {
+
+    //global variable
+    string canYouSeeME = "Yes You Can";
+
+	// Use this for initialization
+	void Start () {
+        string canYouSeeME = "Sure";
+        Debug.Log(canYouSeeME); // use the local variable
+        Debug.Log(this.canYouSeeME); // use the global variable
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void VarMethod()
+    {
+        string canYouSeeMeNow = "I guess you can"; // local variable
+        Debug.Log(canYouSeeMeNow);
+    }
+}
