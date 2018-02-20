07de485c064500e7eac5667c7499f7a716d30099
Mihail Stanescu <mstanescu@ausy-group.com>
2018-02-20 16:22:55 +0200

    Pong tutorial
    -learned how to move from one scene to another using SceneManager
    
    Signed-off-by: Mihail Stanescu

---
+++ Courses/Udemy/3.Our First Game/Pong/Assets/Scripts/TextBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextBehaviour : MonoBehaviour {
+
+    public Text myText;
+
+    private int textNumber = 0;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.Space))
+        {
+            textNumber++;
+            myText.text = textNumber.ToString();
+        }else if (Input.GetKeyDown(KeyCode.S))
+        {
+            myText.text = "S was pressed";
+        }
+            
+    }
+}
