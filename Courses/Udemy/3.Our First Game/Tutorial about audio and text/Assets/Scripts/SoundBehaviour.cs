d8b2bf86cc423e72fc5b454a1a2ab6a56792bf2d
Mihail Stanescu <mstanescu@ausy-group.com>
2018-02-21 19:02:39 +0200

    Test project including essential files
    -includes audio
    -includes text, counters
    -includes scenes and moving from scene to scene and back
    
    Signed-off-by: Mihail Stanescu

---
+++ Courses/Udemy/3.Our First Game/Tutorial about audio and text/Assets/Scripts/SoundBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBehaviour : MonoBehaviour {
+
+    public AudioSource music;
+
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(KeyCode.F))
+        {
+            //start sound
+            if (music)
+            {
+                if (!music.isPlaying)
+                {
+                    music.Play();
+                }
+                else
+                {
+                    music.Stop();
+                }
+            }
+        }
+	}
+}
